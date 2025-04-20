f2c6cc3f5f9bb6e9487cc8ee4d917b63d77a23a0
adamhoward <howard.adam@gmail.com>
2025-04-19 17:39:24 -0700

    Add invnorm

--- clac.c
+++ clac.c
@@ -166,6 +166,47 @@ static double mul(stack *s, int n) {
 	return a;
 }
 
+/* ALH: Adding an invnorm implementation based on Acklam so I can add a word to
+ * calculate Z-scores
+ */
+static double invnorm(double p) {
+    if (p <= 0.0 || p >= 1.0) {
+        return NAN;  // Handle invalid inputs
+    }
+
+    static const double a[] = { -3.969683028665376e+01, 2.209460984245205e+02,
+                                -2.759285104469687e+02, 1.383577518672690e+02,
+                                -3.066479806614716e+01, 2.506628277459239e+00 };
+
+    static const double b[] = { -5.447609879822406e+01, 1.615858368580409e+02,
+                                -1.556989798598866e+02, 6.680131188771972e+01,
+                                -1.328068155288572e+01 };
+
+    static const double c[] = { -7.784894002430293e-03, -3.223964580411365e-01,
+                                -2.400758277161838e+00, -2.549732539343734e+00,
+                                4.374664141464968e+00,  2.938163982698783e+00 };
+
+    static const double d[] = { 7.784695709041462e-03, 3.224671290700398e-01,
+                                2.445134137142996e+00, 3.754408661907416e+00 };
+
+    double q, r;
+
+    if (p < 0.02425) {
+        q = sqrt(-2.0 * log(p));
+        return (((((c[0] * q + c[1]) * q + c[2]) * q + c[3]) * q + c[4]) * q + c[5]) /
+               ((((d[0] * q + d[1]) * q + d[2]) * q + d[3]) * q + 1.0);
+    } else if (p > 1 - 0.02425) {
+        q = sqrt(-2.0 * log(1 - p));
+        return -(((((c[0] * q + c[1]) * q + c[2]) * q + c[3]) * q + c[4]) * q + c[5]) /
+                ((((d[0] * q + d[1]) * q + d[2]) * q + d[3]) * q + 1.0);
+    } else {
+        q = p - 0.5;
+        r = q * q;
+        return (((((a[0] * r + a[1]) * r + a[2]) * r + a[3]) * r + a[4]) * r + a[5]) * q /
+               (((((b[0] * r + b[1]) * r + b[2]) * r + b[3]) * r + b[4]) * r + 1.0);
+    }
+}
+
 static node *get(sds word) {
 	node *curr = head;
 
@@ -415,6 +456,11 @@ static void process(sds word) {
 	} else if (!strcasecmp(word, "erf")) {
 		if (count(s0) > 0) {
 			push(s0, erf(pop(s0)));
+		}
+    /* ALH */
+	} else if (!strcasecmp(word, "invnorm")) {
+		if (count(s0) > 0) {
+			push(s0, invnorm(pop(s0)));
 		}
 	} else if (!strcasecmp(word, "!")) {
 		if (count(s0) > 0) {
